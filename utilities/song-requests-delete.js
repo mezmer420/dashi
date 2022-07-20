e9f156f3ceb301be57e3696f0ad922004bc41de4
mezmer420 <106496025+mezmer420@users.noreply.github.com>
2022-07-20 00:56:16 -0500

    Delete utilities directory

--- utilities/song-requests-delete.js
+++
@@ -1,9 +0,0 @@
-module.exports = async ({ client }) => {
-	client.on("messageCreate", async (message) => {
-		if (message.channel.type == "DM") return
-		if (message.channel.id !== "992630810186367016") return
-		if (message.author.id == "956345939130482750") return
-
-		message.delete().catch((err) => {})
-	})
-}
--- utilities/vc-disconnect.js
+++
@@ -1,7 +0,0 @@
-module.exports = async ({ client }) => {
-    client.player.on("botDisconnect", (queue) => {
-        console.log("disconnected from voice channel")
-
-        queue.destroy().then(console.log("cleared queue"))
-    })
-}
