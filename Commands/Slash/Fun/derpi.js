1038e94f347b634ca44c89ac8b6040daf93a881b
mezmer420 <106496025+mezmer420@users.noreply.github.com>
2022-07-29 12:43:50 -0500

    Add files via upload

--- Commands/Slash/Fun/derpi.js
+++ Commands/Slash/Fun/derpi.js
@@ -5,7 +5,7 @@ const config = require("../../../config.json")
 
 module.exports.data = new SlashCommandBuilder()
 	.setName("derpi")
-	.setDescription("Pick a random pony image from derpibooru!")
+	.setDescription("Pick a random pony image from Derpibooru!")
 	.addStringOption((option) =>
 		option
 			.setName("search-tag-1")
@@ -38,103 +38,105 @@ module.exports.data = new SlashCommandBuilder()
 	)
 
 module.exports.run = async ({ client, interaction }) => {
-	try {
-		const tag1 = interaction.options.getString("search-tag-1")
-		const tag2 = interaction.options.getString("search-tag-2")
-		const tag3 = interaction.options.getString("search-tag-3")
-		const tag4 = interaction.options.getString("search-tag-4")
-		const tag5 = interaction.options.getString("search-tag-5")
+	const tag1 = interaction.options.getString("search-tag-1")
+	const tag2 = interaction.options.getString("search-tag-2")
+	const tag3 = interaction.options.getString("search-tag-3")
+	const tag4 = interaction.options.getString("search-tag-4")
+	const tag5 = interaction.options.getString("search-tag-5")
 
-		let query = tag1
+	let query = tag1
 
-		if (tag2) {
-			query = query.concat(`, ${tag2}`)
-		}
-		if (tag3) {
-			query = query.concat(`, ${tag3}`)
-		}
-		if (tag4) {
-			query = query.concat(`, ${tag4}`)
-		}
-		if (tag5) {
-			query = query.concat(`, ${tag5}`)
-		}
+	if (tag2) {
+		query = query.concat(`, ${tag2}`)
+	}
+	if (tag3) {
+		query = query.concat(`, ${tag3}`)
+	}
+	if (tag4) {
+		query = query.concat(`, ${tag4}`)
+	}
+	if (tag5) {
+		query = query.concat(`, ${tag5}`)
+	}
 
-		async function getImageData(query) {
-			const BASE_URL = "https://derpibooru.org/api/v1/json/search/images?"
+	async function getImageData(query) {
+		const baseURL = "https://derpibooru.org/api/v1/json/search/images?"
 
-			const url =
-				BASE_URL +
-				new URLSearchParams({
-					key: config.derpibooruAPItoken,
-					q: query,
-					per_page: 25,
-					sf: "random",
-				}).toString()
+		const URL =
+			baseURL +
+			new URLSearchParams({
+				key: config.derpibooruAPItoken,
+				q: query,
+				per_page: 25,
+				sf: "random",
+			})
 
-			let response = await fetch(url)
+		let response = await fetch(URL)
 
-			return await response.json().catch((err) => {})
-		}
-
-		const imageData = await getImageData(query)
-
-		if (imageData.total === 0) {
+		if (!response.ok) {
 			return await interaction
 				.editReply({
-					content: `❌ | No results for **${query}**`,
+					conten: "❌ | An error occured",
 				})
 				.catch((err) => {})
 		}
 
-		let images = imageData.images
+		return await response.json().catch((err) => {})
+	}
 
-		images = images.filter((image) => image.tags.includes("safe"))
+	const imageData = await getImageData(query)
 
-		if (images.length === 0) {
-			return await interaction
-				.editReply({
-					content: `❌ | No safe results for **${query}**`,
-				})
-				.catch((err) => {})
-		}
-
-		const image = images[Math.floor(Math.random() * images.length)]
-
-		const id = image.id
-		let tags = image.tags.join(", ")
-		if (tags.length > 50) {
-			tags = tags.substring(0, 47)
-			tags = tags.concat("...")
-		}
-
-		const pageurl = `https://derpibooru.org/images/${id}`
-		const imageurl = image.representations.full
-
-		const Embed = new EmbedBuilder()
-			.setColor("Random")
-			.setTitle(`#${id} - ${tags}`)
-			.setURL(pageurl)
-			.setImage(imageurl)
-			.setFooter({ text: "Derpibooru" })
-
-		if (image.uploader !== null) {
-			const uploader = image.uploader
-			const uploaderurl = `https://derpibooru.org/profiles/${uploader}`
-
-			Embed.setAuthor({ name: uploader, url: uploaderurl })
-		}
-
-		await interaction.editReply({
-			embeds: [Embed],
-		})
-	} catch (err) {
-		await interaction
+	if (imageData.total === 0) {
+		return await interaction
 			.editReply({
-				content: "❌ | An error occured",
+				content: `❌ | No results for **${query}**`,
 			})
 			.catch((err) => {})
-
-		console.log(err)
 	}
+
+	let images = imageData.images
+
+	images = images.filter((image) => image.tags.includes("safe"))
+
+	if (images.length === 0) {
+		return await interaction
+			.editReply({
+				content: `❌ | No safe results for **${query}**`,
+			})
+			.catch((err) => {})
+	}
+
+	const image = images[Math.floor(Math.random() * images.length)]
+
+	const id = image.id
+	let tags = image.tags.join(", ")
+	if (tags.length > 50) {
+		tags = tags.substring(0, 47)
+		tags = tags.concat("...")
+	}
+
+	const pageURL = `https://derpibooru.org/images/${id}`
+	const imageURL = image.representations.full
+
+	const Embed = new EmbedBuilder()
+		.setColor("Random")
+		.setTitle(`#${id} — ${tags}`)
+		.setURL(pageURL)
+		.setImage(imageURL)
+		.setFooter({ text: "Derpibooru" })
+
+	if (image.uploader !== null) {
+		const uploader = image.uploader
+		const uploaderForURL = uploader.replaceAll(" ", "+")
+
+		const uploaderURL = `https://derpibooru.org/profiles/${uploaderForURL}`
+
+		Embed.setAuthor({ name: uploader, url: uploaderURL })
+	}
+
+	await interaction
+		.editReply({
+			embeds: [Embed],
+		})
+		.catch((err) => {})
 }
