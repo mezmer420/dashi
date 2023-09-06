7af49c5295d11dba71b64d988b63a5809dcfeaa1
mezmer420 <findingmoney100@gmail.com>
2023-09-05 21:24:14 -0500

    Update ImageSauce.js

--- src/Commands/Interaction/Context/ImageSauce.js
+++ src/Commands/Interaction/Context/ImageSauce.js
@@ -14,74 +14,91 @@ module.exports.run = async ({ client, interaction, defaultColor }) => {
 	const { targetId } = interaction
 
 	const target = await interaction.channel.messages.fetch(targetId)
-	const data = target.attachments.toJSON()
 
 	let embeds = []
 
-	data.forEach((item) => {
-		if (item.contentType.startsWith("image/")) {
-			const embed = new EmbedBuilder()
-				.setColor(defaultColor)
-				.setTitle(item.name)
-				.setURL(item.url)
-				.addFields(
-					{
-						name: "‎",
-						value: `[Bing](https://www.bing.com/images/searchbyimage?cbir=sbi&imgurl=${item.url})`,
-						inline: true,
-					},
-					{
-						name: "‎",
-						value: `[Google](https://www.google.com/searchbyimage?sbisrc=1&image_url=${item.url})`,
-						inline: true,
-					},
-					{
-						name: "‎",
-						value: `[Google Lens](https://lens.google.com/uploadbyurl?url=${item.url})`,
-						inline: true,
-					},
-					{
-						name: "‎",
-						value: `[ImgOps](https://imgops.com/specialized+reverse/${item.url})`,
-						inline: true,
-					},
-					{
-						name: "‎",
-						value: `[IQDB](https://iqdb.org/?url=${item.url})`,
-						inline: true,
-					},
-					{
-						name: "‎",
-						value: `[Reddit](http://karmadecay.com/search?q=${item.url})`,
-						inline: true,
-					},
-					{
-						name: "‎",
-						value: `[SauceNAO](https://saucenao.com/search.php?db=999&url=${item.url})`,
-						inline: true,
-					},
-					{
-						name: "‎",
-						value: `[TinEye](https://tineye.com/search?url=${item.url})`,
-						inline: true,
-					},
-					{
-						name: "‎",
-						value: `[Yandex](https://yandex.com/images/search?url=${item.url}&rpt=imageview)`,
-						inline: true,
-					}
-				)
-				.setThumbnail(item.url)
+	function createEmbeds(item, type, url) {
+		const embed = new EmbedBuilder()
+			.setColor(defaultColor)
+			.setURL(url)
+			.addFields(
+				{
+					name: "‎",
+					value: `[Bing](https://www.bing.com/images/searchbyimage?cbir=sbi&imgurl=${url})`,
+					inline: true,
+				},
+				{
+					name: "‎",
+					value: `[Google](https://www.google.com/searchbyimage?sbisrc=1&image_url=${url})`,
+					inline: true,
+				},
+				{
+					name: "‎",
+					value: `[Google Lens](https://lens.google.com/uploadbyurl?url=${url})`,
+					inline: true,
+				},
+				{
+					name: "‎",
+					value: `[ImgOps](https://imgops.com/specialized+reverse/${url})`,
+					inline: true,
+				},
+				{
+					name: "‎",
+					value: `[IQDB](https://iqdb.org/?url=${url})`,
+					inline: true,
+				},
+				{
+					name: "‎",
+					value: `[Reddit](http://karmadecay.com/search?q=${url})`,
+					inline: true,
+				},
+				{
+					name: "‎",
+					value: `[SauceNAO](https://saucenao.com/search.php?db=999&url=${url})`,
+					inline: true,
+				},
+				{
+					name: "‎",
+					value: `[TinEye](https://tineye.com/search?url=${url})`,
+					inline: true,
+				},
+				{
+					name: "‎",
+					value: `[Yandex](https://yandex.com/images/search?url=${url}&rpt=imageview)`,
+					inline: true,
+				}
+			)
+			.setThumbnail(url)
 
-			embeds.push(embed)
+		if (type === "attachment") {
+			embed.setTitle(item.name)
+		} else if (type === "embed") {
+			embed.setTitle("Media")
+		}
+
+		embeds.push(embed)
+	}
+
+	const messageAttachments = target.attachments.toJSON()
+
+	messageAttachments.forEach((item) => {
+		if (item.contentType.startsWith("image/")) {
+			createEmbeds(item, "attachment", item.url)
+		}
+	})
+
+	const messageEmbeds = target.embeds
+
+	messageEmbeds.forEach((item) => {
+		if (item.data.type === "image") {
+			createEmbeds(item, "embed", item.data.url)
 		}
 	})
 
 	if (embeds.length === 0) {
 		return await interaction
 			.editReply({
-				content:
-					"Message must contain image(s) (GIFs/videos unsupported)",
+				content: "Message must contain image(s) (videos unsupported)",
 			})
 			.catch((err) => {})
 	}
